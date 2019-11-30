3900402a7322c7ed6de9729ca4fe87ed4989a632
dlapchuk <dalapchuk@gmail.com>
2019-11-30 22:52:00 +0200

    Added new resampling methods

--- server.R
+++ server.R
@@ -1,3 +1,5 @@
+
+
 library(shiny)
 library(data.table)
 library(purrr)
@@ -6,6 +8,8 @@ library(ggplot2)
 library("caret")
 library("data.table")
 library("xgboost")
+library(kohonen)
+
 # use the below options code if you wish to increase the file input limit, in this example file input limit is increased from 5MB to 9MB
 options(shiny.maxRequestSize = 600*1024^2)
 
@@ -31,7 +35,7 @@ shinyServer(function(input,output,session){
     if(is.null(data())){return ()}
     summary(data())
     
-     })
+  })
   
   # This reactive output contains the dataset and display the dataset in table format
   output$table <- renderTable({
@@ -69,15 +73,92 @@ shinyServer(function(input,output,session){
                       
     )
     
-    #resampling function
+    downsample_func <- function(dt,label,coef){
+      
+      pos_cnt <- sum( ifelse(label==1,1,0) )
+      neg_cnt <- sum( ifelse(label==0,1,0) )
+      add_objects <- neg_cnt- floor( neg_cnt-(pos_cnt/coef) )
+      
+      dt$target <- label
+      dt_to_sample <- dt[target==0,]
+      sample_ind <- sample( 1:nrow(dt_to_sample),add_objects,replace=F )
+      
+      dt_to_sample <- dt_to_sample[sample_ind, ]
+      
+      data <- rbindlist (list ( dt_to_sample, dt[target==1,]  ),use.names = T)
+      
+      res <- list()
+      
+      res[["data"]] <- data[,-c("target"),with=F]
+      res[["label"]] <- data$target
+      
+      return(res)
+    }
+    
+    
+    
+    
+    oversample_func <- function(dt,label,coef){
+      
+      pos_cnt <- sum( ifelse(label==1,1,0) )
+      neg_cnt <- sum( ifelse(label==0,1,0) )
+      add_objects <- floor( neg_cnt*coef-pos_cnt)
+      
+      dt$target <- label
+      dt_to_sample <- dt[target==1,]
+      sample_ind <- sample( 1:nrow(dt_to_sample),add_objects,replace=T )
+      
+      dt_to_sample_new <- dt_to_sample[sample_ind, ]
+      dt_to_smaple_new <- rbindlist(list(dt_to_sample_new,dt[target==1,]),use.names = T)
+      remove(dt_to_sample)
+      
+      data <- rbindlist(list(dt_to_smaple_new, dt[target==0,]),use.names = T)
+      
+      remove(dt_to_sample_new, sample_ind)
+      
+      ind <- sample( 1:nrow(data),nrow(data),replace=F )
+      data <- data[ind,]
+      
+      
+      res <- list()
+      
+      res[["data"]] <- data[,-c("target"),with=F]
+      res[["label"]] <- data$target
+      
+      remove(data, ind)
+      
+      return(res)
+    }
+    
+    
     resample_data <- function(dt,labels,type,coef){
+      
       
       coef <- coef
       res <- list()
       
-      if (type=='None'){
+      type <- type
+      
+      print(type)
+      print(coef)
+      
+      if (type=='none'){
         res[["data"]] <- dt
         res[["labels"]] <- labels
+      }
+      
+      if (type=='UR'){
+        
+        resample <- downsample_func (dt,labels,coef)
+        res[["data"]] <- resample[[1]]
+        res[["labels"]] <- resample[[2]]
+      }
+      
+      if (type=='OR'){
+        
+        resample <- oversample_func (dt,labels,coef)
+        res[["data"]] <- resample[[1]]
+        res[["labels"]] <- resample[[2]]
       }
       
       return(res)
@@ -160,32 +241,33 @@ shinyServer(function(input,output,session){
         f1_score_test <- c(f1_score_test, 
                            ( (1+f1_beta^2)*precision_test*recall_test) /( (( f1_beta^2)*precision_test)+recall_test  ) )
       }
-       
+      
       res_cv <- c(mean(accuracy_train), mean(kappa_train), mean(recall_train), mean(precision_train), mean(tn_train),  mean(f1_score_train),
-                   mean(accuracy_test), mean(kappa_test), mean(recall_test), mean(precision_test), mean(tn_test),mean(f1_score_test))
+                  mean(accuracy_test), mean(kappa_test), mean(recall_test), mean(precision_test), mean(tn_test),mean(f1_score_test))
       names(res_cv) <- c("accuracy train","kappa train","recall train", "precision train","true negative train", "f1 score train",
-                          "accuracy test","kappa test","recall test", "precision test","true negative test","f1 score test")
-       
+                         "accuracy test","kappa test","recall test", "precision test","true negative test","f1 score test")
+      
       res <- list()
       res[["metric_result"]] <- res_cv[7:12]
       # 
-      res[["recall_test"]] <- recall_test
+      #res[["recall_test"]] <- recall_test
       #res[["test_res"]] <- 
-      res[["metric_result"]] <- res_cv[7:12]
-      res
+      #res[["metric_result"]] <- res_cv[7:12]
+      #res
       # 
       #test_result
       # res
+      return(res)
       
     }
     
     
     
-    
-    resample_type='None'
-    resample_coef <- 1
+    #None, down, over
+    resample_type <-input$sampling
+    resample_coef <- input$inbCoeff
     pred_cut <- 0.5
-    n_iter <- 30
+    n_iter <- 50
     f1_coef <-1 
     
     
@@ -201,6 +283,13 @@ shinyServer(function(input,output,session){
       resample_type,
       resample_coef
     )
+    
+    #print(xgb_cv_test$metric_result)
+    
+    res_print <- as.data.frame(xgb_cv_test$metric_result)
+    rownames(res_print) <- c("accuracy test","kappa test","recall test","precision test","true negative test","f1 score test ")
+    
+    res_print
     
     
     
@@ -222,12 +311,21 @@ shinyServer(function(input,output,session){
     
     # draw the histogram with the specified number of bins
     #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    data() %>%
-      keep(is.numeric) %>% 
-      gather() %>% 
-      ggplot(aes(value)) +
-      facet_wrap(~ key, scales = "free") +
-      geom_histogram()
+    data_train_matrix <- as.matrix(scale(as.matrix(data()[,-c("ID_code","target"),with=F])))
+    
+    som_grid <- somgrid(xdim = 4, ydim=3, topo="hexagonal")
+    
+    som_model <- som(data_train_matrix, 
+                     grid=som_grid, 
+                     #rlen=500, 
+                     rlen=250,
+                     alpha=c(0.01), 
+                     keep.data = TRUE )
+    
+    
+    coolBlueHotRed <- function(n, alpha = 1) {rainbow(n, end=4/6, alpha=alpha)[n:1]}
+    
+    plot(som_model, type = "property", property = getCodes(som_model)[,4], main=colnames(getCodes(som_model))[4], palette.name=coolBlueHotRed)
     
   })
   
